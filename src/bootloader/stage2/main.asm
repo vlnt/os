6a5c081cc9d50e62af9ed415afff03dabc13e094
Valentyn Mirosh <vlnt@ua.fm>
2022-09-20 17:29:32 +0300

    stage 2 transitional

---
+++ src/bootloader/stage2/main.asm
@@ -0,0 +1,44 @@
+org 0x0
+bits 16
+
+%define ENDL 0x0D, 0x0A
+
+
+
+; Code goes here
+
+
+start:
+    ;print message
+    mov si, msg_hello
+    call puts
+
+.halt:
+    cli
+    hlt
+
+
+;Prints a string to the screen
+;Params:
+;    - ds:si points to the string 
+puts:
+;   save registers we will modify
+    push si
+    push ax
+
+.loop:
+    lodsb           ;loads next character in al
+    or al, al       ;verify if next character is null?
+    jz .done
+
+    mov ah, 0x0e    ;call nios interrupt
+    int 0x10
+
+    jmp .loop
+
+.done:
+    pop ax
+    pop si
+    ret
+
+msg_hello: db 'Hello world from KERNEL!', ENDL, 0
