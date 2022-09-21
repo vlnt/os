e58b40b86edcd9cc9a3c58870d1657f61790da01
Valentyn Mirosh <vlnt@ua.fm>
2022-09-21 22:44:55 +0300

    printf interim

--- src/bootloader/stage2/stdio.c
+++ src/bootloader/stage2/stdio.c
@@ -14,3 +14,69 @@ void puts(const char* str)
         str++;
     }
 }
+
+#define PRINTF_STATE_NORMAL       0
+#define PRINTF_STATE_LENGTH       1
+#define PRINTF_STATE_LENGTH_SHORT 2
+#define PRINTF_STATE_LENGTH_LONG  3
+#define PRINTF_STATE_SPEC         4
+
+#define PRINTF_LENGTH_DEFAULT     0
+#define PRINTF_LENGTH_SHORT_SHORT 1
+#define PRINTF_LENGTH_SHORT       2
+#define PRINTF_LENGTH_LONG        3
+#define PRINTF_LENGTH_LONG_LONG   4
+
+void _cdecl printf(const char* fmt, ...)
+{
+    int* argp = (int*)&fmt;
+    int state = PRINTF_STATE_NORMAL;
+    int length = PRINTF_LENGTH_DEFAULT;
+
+    while(*fmt)
+    {
+        switch(state)
+        {
+            case PRINTF_STATE_NORMAL:
+                switch(*fmt)
+                {
+                    case '%': state = PRINTF_STATE_LENGTH;
+                        break;
+                    default : putc(*fmt); 
+                        break; 
+                }
+                break;
+            case PRINTF_STATE_LENGTH:
+                switch(*fmt)
+                {
+                    case 'h': length = PRINTF_LENGTH_SHORT;
+                              state = PRINTF_STATE_LENGTH_SHORT;
+                        break;
+                    case 'l': length = PRINTF_LENGTH_LONG;
+                              state = PRINTF_STATE_LENGTH_LONG;
+                        break;
+                    default : goto PRINTF_STATE_SPEC_;
+                        break; 
+                }
+                break;
+            case PRINTF_STATE_LENGTH_SHORT:
+                if(*fmt == 'h')
+                {
+                    length = PRINTF_LENGTH_SHORT_SHORT;
+                    state = PRINTF_STATE_SPEC;
+                }
+                else goto PRINTF_STATE_SPEC_;
+                break;
+            case PRINTF_STATE_LENGTH_LONG:
+                if(*fmt == 'h')
+                {
+                    length = PRINTF_LENGTH_LONG_LONG;
+                    state = PRINTF_STATE_SPEC;
+                }
+                else goto PRINTF_STATE_SPEC_;
+                break;
+        }
+
+        fmt++;
+    }
+}
--- src/bootloader/stage2/stdio.h
+++ src/bootloader/stage2/stdio.h
@@ -2,3 +2,5 @@
 
 void putc(char c);
 void puts(const char* str);
+void _cdecl printf(const char* fmt, ...);
+int* print_number(int* argp, int length, bool sign, int radix);
